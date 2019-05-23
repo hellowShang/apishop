548bdfba5722bace3678dd8d2b962a61c7724cdd
hellowShang <13473136348@163.com>
2019-05-23 06:46:30 +0000

    .env文件

---
+++ app/Http/Controllers/Controller/OrderController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    // 订单生成
+    public function orderCreate(){
+
+    }
+}
