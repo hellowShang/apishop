9351d9555901bf4a0cc553611f35bf48046e2f3f
hellowShang <13473136348@163.com>
2019-05-23 12:22:51 +0000

    冲突解决

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('index');
 });
 
 
@@ -11,22 +10,9 @@ Route::get('/cart/cartAdd/','CartController@cartAdd');
 Route::get('/cart/cartList/','CartController@cartList');
 
 
-
-
-
-
-
-
-
-=======
-    return view('index');
-});
-
-
 // 支付宝支付
 Route::get('pay','Controller\PayController@pay');
 // 同步通知
 Route::get('success','Controller\PayController@success');
 // 异步通知
 Route::get('notify','Controller\PayController@notify');
->>>>>>> f51057191359506a4a56c4871c949cda0f5bcc1f
