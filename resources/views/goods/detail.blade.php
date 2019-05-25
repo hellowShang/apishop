d3d7968f05e1aff8bace27f78a0cbb7f0caf448e
Your Name <you@example.com>
2019-05-25 09:38:45 +0800

    修改debug

--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -13,12 +13,12 @@
 		<div class="container">
 			
 			<div class="shop-single">
-				<img src="http://www.lab993.com/uploads/goodsimgs/{{$goods->goods_img}}  " alt="" style="width:30%;height:30%;">
+				<img src="http://www.lab993.com/uploads/goodsimgs/{{$goods->goods_img}}  " alt="" style="width:30%;height:45%;">
 				<h5></h5>
 				<div class="price">￥{{$goods->self_price}} <span>￥{{$goods->market_price}}</span></div>
 				库存：&nbsp;&nbsp;&nbsp;<b>{{$goods->goods_score}}&nbsp;&nbsp;&nbsp;</b>
 				数量：&nbsp;&nbsp;&nbsp;<b>{{$goods->goods_num}}&nbsp;&nbsp;&nbsp;</b><br>
-				<button type="button" class="btn button-default">{{$goods->goods_name}}</button>
+				<button type="button"  class="btn button-default">{{$goods->goods_name}}</button>
 			</div>
 			<div class="review">
 					<h5>{{$goods->goods_desc}}</h5>
